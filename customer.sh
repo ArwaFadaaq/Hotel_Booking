bb9eef8534cf43c2590819d051b2bac4470cd44c
ArawFadaaq <ArawFadaaq@gmail.com>
2024-05-23 00:10:25 +0300

    sumiah

--- customer.sh
+++ customer.sh
@@ -39,10 +39,9 @@ display_products (){
        ;;
   esac
 }
-# Function to add a product to the basket
 add_to_basket() {
   while true; do
-       read -p "Enter the ID of the product you wnat to buy:" product_id
+    read -p "Enter the ID of the product you want to buy: " product_id
 
     # Check if the product ID exists in the products list
     if grep -q "^$product_id," products.txt; then
@@ -72,32 +71,46 @@ add_to_basket() {
       # Add the product ID, name, category, size, and price to the "Basket" file
       printf "%-10s ,%-10s ,%-10s,%-10s,%-10s\n" "$product_id" "$product_name" "$category" "$size" "$price" >> Basket.txt
       echo "Product with ID $product_id (Size: $size) added to Basket."
-      break  # Exit the loop after successfully adding the product
+      
+      while true; do
+        read -p "Do you want to buy more products? (y/n): " buy_more
+        case $buy_more in
+          [Yy]) break ;;
+          [Nn]) return ;;
+          *) echo "Invalid input! Please enter 'y' for yes or 'n' for no." ;;
+        esac
+      done
     else
       echo "Invalid product ID! Please enter a valid ID."
     fi
-    
   done
 }
 
 display_basket(){
+  
+  # Calculate total price using awk
+  total_price=$(awk -F ',' '{total+=$NF} END {print total}' Basket.txt)
+  
+  # Check if total_price is a number
+  if [[ $total_price =~ ^[0-9]+([.][0-9]+)?$ ]]; then
   echo " "
   echo "Your basket contains the following products:"
   cat Basket.txt
-  # Calculate total price using awk
-  total_price=$(awk -F ',' '{total+=$NF} END {print total}' Basket.txt)
-  # Print the total price
   echo "Total Price: $total_price SAR"
+  
+  else
+   echo "****Thank you for visiting our online shopping****"
+   
+  fi
 }
 
-# Function to delete a product from the basket
+
 delete_from_basket() {
   while true; do
     read -p "Enter the ID of the product you want to remove: " product_id
 
     # Check if the product ID exists in the basket
-    if grep -q "^$product_id," Basket.txt; then
-      # Ask for the size of the product to remove
+    if grep -q "^$product_id" Basket.txt; then
       while true; do
         echo "Select the size of the product to remove:"
         echo "1) Small"
@@ -105,7 +118,7 @@ delete_from_basket() {
         echo "3) Large"
         echo "4) XLarge"
         read -p "Enter your choice (1-4): " size_choice
-        
+
         case $size_choice in
           1) size="Small"; break ;;
           2) size="Medium"; break ;;
@@ -115,27 +128,20 @@ delete_from_basket() {
         esac
       done
 
-    # Check if the product with the specified ID and size exists in the basket
-      if awk -F ', ' -v id="$product_id" -v size="$size" '$1 == id && $4 == size' Basket.txt; then
-        # Remove the line associated with the product ID and size from Basket.txt
-        sed -i "/^$product_id ,.* ,.* , $size ,/d" Basket.txt
-        echo "Product with ID $product_id and size $size removed from the basket."
-
-        # Check if there are remaining products in the basket
-        if [ $(awk 'NR > 1' Basket.txt | wc -l) -gt 0 ]; then
-          while true; do
-            read -p "Do you want to delete more products? (y/n): " delete_more
-            case $delete_more in
-              [Yy]) break ;;
-              [Nn]) break 2 ;;
-              *) echo "Invalid input! Please enter 'y' for yes or 'n' for no." ;;
-            esac
-          done
-        else
-          echo "No more products in the basket."
-          break
-        fi
-      else
+     if awk "/^$product_id/ && /$size/" Basket.txt >/dev/null; then
+     
+       sed -i "/^$product_id/ { /$size/ d }" Basket.txt 
+        echo "Product with ID $product_id (Size: $size) removed from the basket."
+        
+        while true; do
+          read -p "Do you want to delete another product? (y/n): " delete_another
+          case $delete_another in
+            [Yy]) continue 2 ;;
+            [Nn]) return ;;
+            *) echo "Invalid input! Please enter 'y' for yes or 'n' for no." ;;
+          esac
+        done
+      else 
         echo "Product with ID $product_id and size $size not found in the basket!"
       fi
     else
@@ -143,7 +149,52 @@ delete_from_basket() {
     fi
   done
 }
-  printf "%-10s ,%-10s ,%-10s ,%-10s,%-10s\n" "Product ID" "Product Name" "Category" "Size" "Price(SAR)" > Basket.txt
+# Function for basket options
+basket_options() {
+  while true; do
+    echo " "
+    echo "**********************************************"  
+    echo "*              Basket Options                *"
+    echo "*                                            *"  
+    echo "**********************************************"  
+    echo "  1) Confirm order"
+    echo "  2) Buy more"
+    echo "  3) Delete product"
+    echo "  4) Exit"
+
+    # Read basket option with validation loop
+    while true; do
+      read -p "Enter your choice: " basket_option
+
+      # Check if basket option is a valid number between 1 and 4 (inclusive)
+      if [[ "$basket_option" =~ ^[1-4]$ ]]; then
+        break;
+      else
+        echo "Invalid choice! Please enter a number between 1 and 4."
+      fi
+    done
+
+    case $basket_option in
+      1) 
+        echo "Order confirmed. Thank you for shopping with us!"
+        display_basket
+        exit 0
+        ;;
+      2) 
+        add_to_basket
+        ;;
+      3) 
+        delete_from_basket
+        ;;
+      4) 
+        echo "Exiting."
+        rm Basket.txt
+        exit 0
+        ;;
+    esac
+  done
+}
+ printf "%-10s ,%-10s ,%-10s ,%-10s,%-10s\n" "Product ID" "Product Name" "Category" "Size" "Price(SAR)" > Basket.txt
 
 # Main loop
 while true; do
@@ -177,84 +228,11 @@ while true; do
   else
     # Display products based on valid choice
     display_products "$choice"
-   items_added=false
-
-  while true; do
-    read -p "Do you want to buy? (y/n): " buy
-    case $buy in
-      [Yy]) 
-         add_to_basket
-          items_added=true
-          while true; do
-          read -p "Do you want to buy more products? (y/n): " buy_more
-          case $buy_more in
-            [Yy]) add_to_basket;;
-            [Nn]) break 2 ;;
-            *) echo "Invalid input! Please enter 'y' for yes or 'n' for no." ;;
-            esac
-            done ;;
-      [Nn]) 
-            break 
-        ;;
-      *) 
-        echo "Invalid input! Please enter 'y' for yes or 'n' for no."
-        ;;
-    esac
-    
-  done
-
- # If no items were added, return to the start of the main loop
- if ! "$items_added"; then
-   continue
-  fi
+    add_to_basket
+    fi
  
- display_basket
- # Display basket options
-    echo " "
-    echo "**********************************************" 
-    echo "*              Basket Options                *"
-    echo "*                                            *"  
-    echo "**********************************************" 
-    echo "  1) Confirm order"
-    echo "  2) Buy more"
-    echo "  3) Delete product"
-    echo "  4) Exit"
-
-    # Read basket option with validation loop
-    while true; do
-      read -p "Enter your choice: " basket_option
-
-      # Check if basket option is a valid number between 1 and 4 (inclusive)
-      if [[ "$basket_option" =~ ^[1-4]$ ]]; then
-        break;
-      else
-        echo "Invalid choice! Please enter a number between 1 and 4."
-      fi
-    done
-
-    case $basket_option in
-      1) 
-        echo "****Order confirmed. Thank you for shopping with us!***"
-        display_basket
-        exit 0
-        ;;
-      2) 
-      continue
-        ;;
-      3) 
-        delete_from_basket
-        echo "****Order confirmed. Thank you for shopping with us!***"
-        display_basket
-        rm Basket.txt
-        exit 0
-        ;;
-      4) 
-        echo "Exit"
-        rm Basket.txt
-        exit 0
-        ;;
-    esac
-  fi
+  display_basket
+  basket_options
 done
   
   
