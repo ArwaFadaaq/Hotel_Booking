66352043dead082158dbb88debe04f3da92aee5e
ArwaFadaaq <s444015416@st.uqu.edu.sa>
2024-05-23 06:35:52 +0300

    kawthr

--- main.sh
+++ main.sh
@@ -23,7 +23,7 @@ customer_login() {
     echo "*             Customer Login                 *"
     echo "*                                            *"
     echo "**********************************************"
-<<<<<<< HEAD
+
     read -p "Enter your name: " customer_name
 
     while true; do
@@ -37,13 +37,6 @@ customer_login() {
 
     echo "Welcome back, $customer_name! You're now logged in."
     read -p "Press Enter to return to main menu" enter_key
-=======
-    read -p "Enter your phone number: " phone_number
-    # For demonstration purposes, any phone number is accepted
-    echo "Welcome back, dear customer! You're now logged in."
-    sleep 2
-    sh customer.sh
->>>>>>> 25b5ca4c46c61c33b71d6a31215e81b2fd19f057
 }
 
 # Function to handle admin login
@@ -69,7 +62,6 @@ admin_login() {
     stty echo
 
     echo "" # For newline after password input
-<<<<<<< HEAD
 
     # Define valid admin credentials
     declare -A admin_credentials
@@ -80,15 +72,12 @@ admin_login() {
     # Check if the entered credentials are valid
     if [[ -n "${admin_credentials[$admin_id]}" && "${admin_credentials[$admin_id]}" == "$admin_password" ]]; then
         echo "Welcome, $admin_id! You're now logged in."
-=======
-    # Add your admin login logic here
-    # For demonstration purposes, let's assume a fixed admin ID and password
-    if [ "$admin_id" = "admin" ] && [ "$admin_password" = "admin123" ]; then
-        echo "Welcome, Administrator! You're now logged in."
->>>>>>> 25b5ca4c46c61c33b71d6a31215e81b2fd19f057
-        sh admin.sh
+        read -p "Press Enter to continue" enter_key
+        # Here you can call another script or continue with admin tasks
+        # For example: sh admin.sh
     else
         echo "Invalid ID or password. Please try again."
+        read -p "Press Enter to return to admin login" enter_key
     fi
 }
 
@@ -112,11 +101,7 @@ main() {
                 ;;
             *)
                 echo "Invalid choice. Please enter a valid option."
-<<<<<<< HEAD
                 read -p "Press Enter to return to main menu" enter_key
-=======
-                sleep 2
->>>>>>> 25b5ca4c46c61c33b71d6a31215e81b2fd19f057
                 ;;
         esac
     done
